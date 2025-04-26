b502a525bfb334559a00ec647d7b38f5dc380cd1
lis <liza850l@yandex.ru>
2025-04-26 12:26:30 +0300

    Добавлена последовательность an, добавлен файл gitignore с *.class

--- momo/J.java
+++ momo/J.java
@@ -1,5 +1,9 @@
 public class J {
     public static void main(String[] args) {
-        System.out.print("Jopa");
+        System.out.println("Jopa");
+        String[] family = { "mama", "papa", "polya", "liza" };
+        for (String i : family) {
+            System.out.println(i);
+        }
     }
 }
--- python.py
+++ python.py
@@ -1,4 +1,19 @@
 print("Hello world")
-for i in range:
-    print("a"*100)
-make some problems
+for i in range (10):
+    print("a"*i)
+
+a = [2, 2]
+j = 1
+for i in range (10):
+    j += 1
+    b = 2*a[j-1] + a[j-2]
+    a.append(b)
+print(a)
+
+c = [2, 2]
+c1 = 1-2**(1/2)
+c2 = 1+2**(1/2)
+for i in range (2, 12):
+    f = c1**i + c2**i
+    c.append(f)
+print(c)
